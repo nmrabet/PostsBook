c31f8f0c26d89cca129145f3b261a2065bff5c8c
Nanouss <mrabet_nesrine@yahoo.fr>
2020-12-21 06:06:19 +0100

    Display one comment

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -1,34 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { Comment, Post } from "types";
 import { useParams } from "react-router-dom";
-import './postDetails.css';
-import {
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Button
-} from "reactstrap";
+import "./postDetails.css";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 export default function PostDetails() {
   const params = useParams<{ postId: string }>();
 
   const [comments, setComments] = useState<Comment[]>([]);
-  const [post, setPost] = useState();
+  const [post, setPost] = useState<Post>();
 
   const urlPost = "https://jsonplaceholder.typicode.com/posts/" + params.postId;
 
   const url =
     "https://jsonplaceholder.typicode.com/comments?postId=" + params.postId;
 
-  
-
   useEffect(() => {
     fetch(urlPost)
       .then((res) => res.json())
       .then((data) => {
-        setPost(data)});
-  },);
+        setPost(data);
+      });
+  });
 
   useEffect(() => {
     fetch(url)
@@ -39,18 +32,28 @@ export default function PostDetails() {
   return (
     <div className='post-details'>
       <div className='post-content'>
-     
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <h3>{post.body}</h3>
+        </div>
       </div>
       <div>
-      <Form>
-      <FormGroup className='form'>
-        <Label>Email</Label>
-        <Input type="text" name="email" placeholder="Enter your Email" required></Input>
-        <Label>Text</Label>
-        <textarea name="text" placeholder="Enter text" required></textarea>
-      <Button type="submit" className='btn'>Submit</Button>
-      </FormGroup>
-      </Form>
+        <Form>
+          <FormGroup className='form'>
+            <Label>Email</Label>
+            <Input
+              type='text'
+              name='email'
+              placeholder='Enter your Email'
+              required
+            ></Input>
+            <Label>Text</Label>
+            <textarea name='text' placeholder='Enter text' required></textarea>
+            <Button type='submit' className='btn'>
+              Submit
+            </Button>
+          </FormGroup>
+        </Form>
       </div>
       <div className='comments'>
         {comments.map((comment) => {
