ce90c4ba2549fdd1eae624ac20e1acb8abfcebf6
Nanouss <mrabet_nesrine@yahoo.fr>
2020-12-21 15:07:13 +0100

    Post display

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -2,25 +2,22 @@ import React, { useState, useEffect } from "react";
 import { Comment, Post } from "types";
 import { useParams } from "react-router-dom";
 import "./postDetails.css";
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 export default function PostDetails() {
   const params = useParams<{ postId: string }>();
-
   const [comments, setComments] = useState<Comment[]>([]);
-  const [post, setPost] = useState<Post>();
+  const [post, setPost] = useState<Post | null>();
+  // const [email, setEmail] = useState('');
+  // const [body, setBody] = useState('');
 
   const urlPost = "https://jsonplaceholder.typicode.com/posts/" + params.postId;
-
   const url =
     "https://jsonplaceholder.typicode.com/comments?postId=" + params.postId;
 
   useEffect(() => {
     fetch(urlPost)
       .then((res) => res.json())
-      .then((data) => {
-        setPost(data);
-      });
+      .then((data) => setPost(data));
   });
 
   useEffect(() => {
@@ -30,31 +27,14 @@ export default function PostDetails() {
   }, [url]);
 
   return (
-    <div className='post-details'>
-      <div className='post-content'>
-        <div key={post.id}>
+    <div className='postDetails'>
+      {post && (
+        <div className='post-content'>
           <h3>{post.title}</h3>
-          <h3>{post.body}</h3>
+          <h3>{post.title}</h3>
         </div>
-      </div>
-      <div>
-        <Form>
-          <FormGroup className='form'>
-            <Label>Email</Label>
-            <Input
-              type='text'
-              name='email'
-              placeholder='Enter your Email'
-              required
-            ></Input>
-            <Label>Text</Label>
-            <textarea name='text' placeholder='Enter text' required></textarea>
-            <Button type='submit' className='btn'>
-              Submit
-            </Button>
-          </FormGroup>
-        </Form>
-      </div>
+      )}
+
       <div className='comments'>
         {comments.map((comment) => {
           return (
